a99356dbddbb9cd9c79d1e242be027b4fbe97978
Ahsan <ahsan44411@gmail.com>
2021-03-11 17:17:27 +0500

    css and navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Home from './containers/Home'
 import Listings from './containers/Listings'
@@ -13,24 +13,28 @@ import NotFound from "./components/NotFound";
 import Layout from './hocs/Layout'
 
 import './sass/main.scss';
+import {Provider} from 'react-redux';
+import store from "./store";
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/contact' component={Contact} />
-                <Route exact path='/listings' component={Listings} />
-                <Route exact path='/listing/:id' component={ListingDetail} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={Signup} />
-                <Route component={NotFound} />
-            </Switch>
-        </Layout>
-    </Router>
-);
 
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/about' component={About}/>
+                    <Route exact path='/contact' component={Contact}/>
+                    <Route exact path='/listings' component={Listings}/>
+                    <Route exact path='/listing/:id' component={ListingDetail}/>
+                    <Route exact path='/login' component={Login}/>
+                    <Route exact path='/signup' component={Signup}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
+);
 
 
 export default App;
